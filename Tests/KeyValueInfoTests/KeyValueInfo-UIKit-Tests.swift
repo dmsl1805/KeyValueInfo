1c85ee5d412a7a6f5cad1ffc68870d2b58ff638e
Dmytro Shulzhenko <dmytro.shulzhenko@gen.tech>
2021-08-23 17:27:40 +0300

    update uikit tests

--- Tests/KeyValueInfoTests/KeyValueInfo-UIKit-Tests.swift
+++ Tests/KeyValueInfoTests/KeyValueInfo-UIKit-Tests.swift
@@ -1,13 +1,22 @@
 #if canImport(UIKit)
 import XCTest
+import CwlPreconditionTesting
 
 import KeyValueInfo
 import KeyValueInfo_UIKit
 
 final class CustomTableViewCell: UITableViewCell { }
+final class CustomHeaderFooterTableViewCell: UITableViewHeaderFooterView { }
+
+final class OtherTableViewCell: UITableViewCell { }
+final class OtherHeaderFooterTableViewCell: UITableViewHeaderFooterView { }
 
 extension KeyValueInfo where Key == String {
     static var tableCell: KeyValueInfo<Key, CustomTableViewCell> { #function }
+    static var tableHeaderFooterCell: KeyValueInfo<Key, CustomHeaderFooterTableViewCell> { #function }
+    
+    static var otherCell: KeyValueInfo<Key, OtherTableViewCell> { "tableCell" }
+    static var otherHeaderFooterCell: KeyValueInfo<Key, OtherHeaderFooterTableViewCell> { "tableHeaderFooterCell" }
 }
 
 final class KeyValueInfoUIKitTests: XCTestCase {
@@ -21,6 +30,39 @@ final class KeyValueInfoUIKitTests: XCTestCase {
         
         _ = { () -> CustomTableViewCell in
             tableView.dequeueReusableCell(withIdentifier: .tableCell, for: IndexPath(index: 0))
+        }
+        
+        tableView.register(
+            CustomHeaderFooterTableViewCell.self,
+            forHeaderFooterViewReuseIdentifier: .tableHeaderFooterCell
+        )
+        
+        _ = { () -> CustomHeaderFooterTableViewCell in
+            tableView.dequeueReusableHeaderFooterView(withIdentifier: .tableHeaderFooterCell)
+        }
+    }
+    
+    func testFailureType() {
+        let tableView = UITableView()
+        let userDefaults = UserDefaults.standard
+        let identifier = ""
+        let indexPath = IndexPath(index: 0)
+        
+        
+        
+        _ = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomTableViewCell
+        userDefaults.setValue(123, forKey: "key")
+        userDefaults.setValue(true, forKey: "key")
+            
+        tableView.register(
+            OtherTableViewCell.self,
+            forCellReuseIdentifier: .otherCell
+        )
+        
+        _ = catchBadInstruction {
+            _ = { () -> CustomTableViewCell in
+                tableView.dequeueReusableCell(withIdentifier: .tableCell, for: IndexPath(index: 0))
+            }()
         }
     }
 }
